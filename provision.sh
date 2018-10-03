45e416bab73be91972f9e1b029c38bc355400a87
Loïc Nogues <nogues.loic@gmail.com>
2018-10-03 11:59:51 +0200

    Updated provision script

--- provision.sh
+++ provision.sh
@@ -20,7 +20,7 @@ sudo pg_createcluster 9.5 main --start
 
 # Ruby 2.4.2
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable --ruby=2.4.2
+curl -sSL https://get.rvm.io | bash -s stable --ruby=2.4.4
 source ~/.rvm/scripts/rvm
 gem install bundler
 
